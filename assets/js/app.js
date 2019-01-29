bac58ee9fc8500d2b347f0af05b4080f10f200d8
johnny59h <lucas.zheng@warriorlife.net>
2019-01-29 09:38:29 -0800

    randomcard
    gets a random card to appear when btn clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,26 @@
+var deckOfCards = [];
+var deck = document.querySelector('.deck');
+var cardInPlay = document.querySelector('.play');
+
+
+deck.addEventListener('click', function(e) {
+  // e.target <-- thing you clicked on
+  // e.target.className <-- class of the thing
+  // e.target.style.display = "none" <-- hides the thing
+  var nextCard = deckOfCards.pop();
+  if(nextCard !== undefined) {
+    cardInPlay.src = "img/cards/1x/" + nextCard + ".png";
+  }
+});
+
+cardInPlay.addEventListener('click', function(e) {
+  var src = e.target.src;
+  alert('You clicked on a card in play. It\'s file is ' + src);
+});
+
+
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
-
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
 
@@ -16,15 +36,11 @@ function shuffle(array) {
 
   return array;
 }
-
-// Used like so
-
 var cardsArr = ["club_jack", "club_queen", "club_king", "diamond_jack", "diamond_queen", "diamond_king", "heart_jack", "heart_queen", "heart_king", "spade_jack", "spade_queen", "spade_king"];
-for (i=0; i<10; i++) {
+for (i=1; i<=10; i++) {
   cardsArr.push("club_" + i);
   cardsArr.push("diamond_" + i);
   cardsArr.push("heart_" + i);
   cardsArr.push("spade_" + i);
 }
-cardsArr = shuffle(cardsArr);
-console.log(cardsArr);
+deckOfCards = shuffle(cardsArr);
