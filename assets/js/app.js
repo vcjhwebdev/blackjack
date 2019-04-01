35e9e4171dc341c37fd2b2a7b0500ea1f1f6d0fa
johnny59h <lucas.zheng@warriorlife.net>
2019-04-01 09:50:31 -0700

    sound attempt

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,23 @@ var endGame = true
 var numOfPlayerCards = 0;
 var numOfDealerCards = 0;
 var score = 0;
+var mySound;
+var myMusic;
+
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
 //var hasAceP = false
 //var hasAceD = false
 
@@ -23,6 +40,8 @@ var score = 0;
 
 
 //})
+myMusic = new sound("gametheme.mp3");
+myMusic.play()
 
 function placeModal(content) {
   var modal = document.createElement('div');
